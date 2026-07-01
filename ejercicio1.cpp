1f738a42192ec3e8c31c78772c13c9fa49cf97d6
Valeriano <mvalerianom@unjbg.edu.pe>
2026-06-30 23:19:48 -0500

    Modifique y agregué la librería de vector para facilitarme

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -1,13 +1,27 @@
-#include <iostreamm>
+#include <iostream>
 #include <string>
+#include <vector> //salvavidas [?]
 using namespace std;
 
 struct Empleado {
-	string nombre;
+	string nombres;
 	char sexo;
 	double sueldo;
 };
 
 int main(){
+	int cantidad;
+	
+	cout << "Ingrese la cantida de trabajadores a registrar: ";
+	cin >> cantidad;
+	
+	vector<Empleado> listaEmpleados(cantidad);
+	
+	for (int i = 0 ;i < cantidad; i++){//Te saltarías por completo al primer empleado
+		cout << "\n---Datos del Empleado " << i + 1 << " --- " << endl;
+		cout << "Nombres: ";
+		cin.ignore();
+		getline(cin, listaEmpleados[i].nombres);
+	}
 	
 }
